ee3e7f502eda902d3026abde627e6371b0dcbd4d
Alvaro Tedesco <atdesenvolvimento2x@gmail.com>
2026-04-13 02:21:05 -0300

    style: implement mobile-first responsive layout with breakpoints and hamburger menu

--- js/index.js
+++ js/index.js
@@ -1,92 +1,119 @@
 const companyExperiences = [
-    {
-        companyName: "CoverPay",
-        image: "https://placehold.co/200x111",
-        startDate: "03/2022",
-        endDate: "04/2024",
-        isCurrentJob: false,
-        details: "",
-    },
-    {
-        companyName: "TOTVS Oeste",
-        image: "https://placehold.co/200x111",
-        startDate: "05/2024",
-        endDate: "",
-        isCurrentJob: true,
-        details: "",
-    },
+  {
+    companyName: "CoverPay",
+    image: "https://placehold.co/200x111",
+    startDate: "03/2022",
+    endDate: "04/2024",
+    isCurrentJob: false,
+    details: "",
+  },
+  {
+    companyName: "TOTVS Oeste",
+    image: "https://placehold.co/200x111",
+    startDate: "05/2024",
+    endDate: "",
+    isCurrentJob: true,
+    details: "",
+  },
 ];
 
 function loadSettings() {
-    setCurrentYear();
-    setHeroBannerImage();
-    setCompanyExperiences();
+  setCurrentYear();
+  setHeroBannerImage();
+  setCompanyExperiences();
+
+  const mobileMenuContainer = document.querySelector(".mobileMenuContainer");
+
+  setClickEvent(mobileMenuContainer, function () {
+    mobileMenuContainer.classList.toggle("active");
+
+    const navList = document.querySelector(".navList");
+
+    if (!navList) {
+      return;
+    }
+
+    navList.classList.toggle("active");
+  });
 }
 
 function setCurrentYear() {
-    const currentYearContainer = document.getElementById("currentYearContainer");
+  const currentYearContainer = document.getElementById("currentYearContainer");
 
-    if (!currentYearContainer) {
-        console.warn("f. setCurrentYear - Error when attempting to retrieve the element \"currentYearContainer\"!");
+  if (!currentYearContainer) {
+    console.warn(
+      'f. setCurrentYear - Error when attempting to retrieve the element "currentYearContainer"!',
+    );
 
-        return false;
-    }
+    return;
+  }
 
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    currentYearContainer.innerText = currentDate.toLocaleDateString("pt-BR", { year: "numeric" });
-
-    return true;
+  currentYearContainer.innerText = currentDate.toLocaleDateString("pt-BR", {
+    year: "numeric",
+  });
 }
 
 function setHeroBannerImage() {
-    const heroBannerImage = document.getElementById("heroBannerImage");
+  const heroBannerImage = document.getElementById("heroBannerImage");
 
-    if (!heroBannerImage) {
-        console.warn("f. setHeroBannerImage - Error when attempting to retrieve the element \"heroBannerImage\"!");
+  if (!heroBannerImage) {
+    console.warn(
+      'f. setHeroBannerImage - Error when attempting to retrieve the element "heroBannerImage"!',
+    );
 
-        return false;
-    }
+    return;
+  }
 
-    heroBannerImage.innerHTML = `<img src="https://placehold.co/250x300" alt="perfilImageTest" style="max-width: 100%;" loading="lazy">`;
-
-    return true;
+  heroBannerImage.innerHTML = `<img src="https://placehold.co/250x300" alt="perfilImageTest" style="max-width: 100%;" loading="lazy">`;
 }
 
 function setCompanyExperiences() {
-    const carouselCompanyExperienceItemsContainer = document.getElementById("carouselCompanyExperienceItemsContainer");
+  const carouselCompanyExperienceItemsContainer = document.getElementById(
+    "carouselCompanyExperienceItemsContainer",
+  );
 
-    if (!carouselCompanyExperienceItemsContainer) {
-        console.warn("f. setCompanyExperiences - Error when attempting to retrieve the element \"carouselCompanyExperienceItemsContainer\"!");
+  if (!carouselCompanyExperienceItemsContainer) {
+    console.warn(
+      'f. setCompanyExperiences - Error when attempting to retrieve the element "carouselCompanyExperienceItemsContainer"!',
+    );
 
-        return false;
-    }
+    return;
+  }
 
-    const carouselCompanyExperienceItems = [];
+  const carouselCompanyExperienceItems = [];
 
-    for (const companyExperience of companyExperiences) {
-        carouselCompanyExperienceItems.push(`<div class="carouselCompanyExperienceItem">
-            <div class="carouselCompanyExperienceItemImage">
-                <img src="${companyExperience.image}" alt="perfilImageTest" style="max-width: 100%;" loading="lazy">
-            </div>
+  for (const companyExperience of companyExperiences) {
+    carouselCompanyExperienceItems.push(`<div class="carouselCompanyExperienceItem">
+      <div class="carouselCompanyExperienceItemImage">
+        <img src="${companyExperience.image}" alt="perfilImageTest" style="max-width: 100%;" loading="lazy">
+      </div>
 
-            <div class="carouselCompanyExperienceItemDetails">
-                <h4 class="carouselCompanyExperienceItemDetailsTitle">${companyExperience.companyName}</h4>
+      <div class="carouselCompanyExperienceItemDetails">
+        <h4 class="carouselCompanyExperienceItemDetailsTitle">${companyExperience.companyName}</h4>
 
-                <span class="carouselCompanyExperienceItemDetailsStart">Iniciado em ${companyExperience.startDate}</span>
+        <span class="carouselCompanyExperienceItemDetailsStart">Iniciado em ${companyExperience.startDate}</span>
 
-                <br>
+        <br>
 
-                <span class="carouselCompanyExperienceItemDetailsEnd">${companyExperience.isCurrentJob ? "Cargo Atual" : `Finalizado em ${companyExperience.endDate}`}</span>
+        <span class="carouselCompanyExperienceItemDetailsEnd">${companyExperience.isCurrentJob ? "Cargo Atual" : `Finalizado em ${companyExperience.endDate}`}</span>
 
-                <br>
+        <br>
 
-                <button class="carouselCompanyExperienceItemDetailsButton" type="button">Mais detalhes</button>
-            </div>
-        </div>`);
-    }
+        <button class="carouselCompanyExperienceItemDetailsButton" type="button">Mais detalhes</button>
+      </div>
+    </div>`);
+  }
 
-    carouselCompanyExperienceItemsContainer.innerHTML = carouselCompanyExperienceItems.join("");
+  carouselCompanyExperienceItemsContainer.innerHTML =
+    carouselCompanyExperienceItems.join("");
+}
 
-    return true;
+function setClickEvent(element, callback) {
+  if (!element) {
+    return;
+  }
+
+  element.addEventListener("click", callback);
 }
